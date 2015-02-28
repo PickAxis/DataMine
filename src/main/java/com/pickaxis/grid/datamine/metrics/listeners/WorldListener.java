3ab828bb7315b299cc3b693e89656a70936d5fdf
NickG365 <nick@stormjive.com>
2015-02-27 19:03:18 -0500

    Track entity spawns.

--- src/main/java/com/pickaxis/grid/datamine/metrics/listeners/WorldListener.java
+++ src/main/java/com/pickaxis/grid/datamine/metrics/listeners/WorldListener.java
@@ -4,6 +4,7 @@ import com.github.arnabk.statsd.Priority;
 import com.pickaxis.grid.datamine.DataMinePlugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.world.WorldLoadEvent;
 import org.bukkit.event.world.WorldUnloadEvent;
 
@@ -36,5 +37,12 @@ public class WorldListener extends AbstractMetricListener
         
         String message = "[Unloaded] World " + event.getWorld().getName() + " has been unloaded on " + this.getInstanceName();
         this.getEventClient().event( message, message, Priority.low );
+    }
+    
+    @EventHandler( priority = EventPriority.MONITOR, 
+                   ignoreCancelled = true )
+    public void onEntitySpawn( EntitySpawnEvent event )
+    {
+        this.getClient().increment( "world.entity_spawns", "type:" + event.getEntityType().name() );
     }
 }
