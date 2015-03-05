55e07096d6724bec0ee4d26e92b27e7c40ba499e
NickG365 <nick@stormjive.com>
2015-03-05 01:31:39 -0500

    Rename that.

--- src/main/java/com/pickaxis/grid/datamine/DataMineCommand.java
+++ src/main/java/com/pickaxis/grid/datamine/DataMineCommand.java
@@ -19,36 +19,36 @@ public class DataMineCommand implements CommandExecutor
     @Override
     public boolean onCommand( CommandSender sender, Command command, String label, String[] args )
     {
-        DataMinePlugin gridvr = DataMinePlugin.getInstance();
+        DataMinePlugin dm = DataMinePlugin.getInstance();
         
         switch( label )
         {
             case "datamine":
-                this.message( sender, "DataMine v" + gridvr.getDescription().getVersion() + " (" +
-                                      gridvr.getBuildInfo().getProperty( "git.branch" ) + "/" +
-                                      gridvr.getBuildInfo().getProperty( "git.commit.id.describe" ) + ")" );
+                this.message( sender, "DataMine v" + dm.getDescription().getVersion() + " (" +
+                                      dm.getBuildInfo().getProperty( "git.branch" ) + "/" +
+                                      dm.getBuildInfo().getProperty( "git.commit.id.describe" ) + ")" );
 
                 String builtBy = "";
                 if( sender.hasPermission( "grid.cmd.datamine.info.extended" ) )
                 {
-                    builtBy = "by " + gridvr.getBuildInfo().getProperty( "git.build.user.name" );
+                    builtBy = "by " + dm.getBuildInfo().getProperty( "git.build.user.name" );
                 }
                 this.message( sender, "Built " + builtBy + " on " +
-                                      gridvr.getBuildInfo().getProperty( "git.build.time" ) );
+                                      dm.getBuildInfo().getProperty( "git.build.time" ) );
 
                 if( sender.hasPermission( "grid.cmd.datamine.info.extended" ) )
                 {
-                    this.message( sender, "Full Commit Hash: " + gridvr.getBuildInfo().getProperty( "git.commit.id" ) );
-                    this.message( sender, "Commit Message: " + gridvr.getBuildInfo().getProperty( "git.commit.message.short" ) );
-                    this.message( sender, "Committed by " + gridvr.getBuildInfo().getProperty( "git.commit.user.name" ) + " <" +
-                                          gridvr.getBuildInfo().getProperty( "git.commit.user.email" ) + "> on " +
-                                          gridvr.getBuildInfo().getProperty( "git.commit.time" ) );
+                    this.message( sender, "Full Commit Hash: " + dm.getBuildInfo().getProperty( "git.commit.id" ) );
+                    this.message( sender, "Commit Message: " + dm.getBuildInfo().getProperty( "git.commit.message.short" ) );
+                    this.message( sender, "Committed by " + dm.getBuildInfo().getProperty( "git.commit.user.name" ) + " <" +
+                                          dm.getBuildInfo().getProperty( "git.commit.user.email" ) + "> on " +
+                                          dm.getBuildInfo().getProperty( "git.commit.time" ) );
                 }
                 break;
             
             case "lag":
-                gridvr.getStatsd().increment( "players.lag_reports", "player:" + ( (Player) sender ).getUniqueId().toString() + "/" + sender.getName() );
-                this.message( sender, LangUtil.getString( "lagreport", "&eYour lag report has been successfully submitted to our server engineers.", gridvr) );
+                dm.getStatsd().increment( "players.lag_reports", "player:" + ( (Player) sender ).getUniqueId().toString() + "/" + sender.getName() );
+                this.message( sender, LangUtil.getString( "lagreport", "&eYour lag report has been successfully submitted to our server engineers.", dm) );
                 break;
         }
         
