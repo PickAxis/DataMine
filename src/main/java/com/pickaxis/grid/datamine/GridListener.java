bc5f9cf961278de83ab11aa4072e4c37ea132bb3
NickG365 <nick@stormjive.com>
2015-04-29 13:00:45 -0400

    Don't attempt to initialize again if Grid is reloaded.

--- src/main/java/com/pickaxis/grid/datamine/GridListener.java
+++ src/main/java/com/pickaxis/grid/datamine/GridListener.java
@@ -3,6 +3,7 @@ package com.pickaxis.grid.datamine;
 import com.pickaxis.grid.core.events.GridInitializedEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 
 /**
@@ -17,5 +18,8 @@ public class GridListener implements Listener
     public void onGridInitializedEvent( GridInitializedEvent event )
     {
         DataMinePlugin.getInstance().initialize();
+        
+        // Don't attempt to initialize again if Grid is reloaded.
+        HandlerList.unregisterAll( this );
     }
 }
