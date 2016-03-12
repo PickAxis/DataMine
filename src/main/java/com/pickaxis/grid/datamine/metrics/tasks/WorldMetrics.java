e2c6e19b69832d3ab1719e33802cb17f326ae2c2
NickG365 <nick@stormjive.com>
2016-03-12 18:04:57 -0500

    Revert "Send tile entity counts with the async task instead."
    This reverts commit e48877a5cc80ffb0a0ac0dfcb6b542606ec3ef73.

--- src/main/java/com/pickaxis/grid/datamine/metrics/tasks/WorldMetrics.java
+++ src/main/java/com/pickaxis/grid/datamine/metrics/tasks/WorldMetrics.java
@@ -1,10 +1,10 @@
 package com.pickaxis.grid.datamine.metrics.tasks;
 
 import com.pickaxis.grid.datamine.DataMinePlugin;
-import com.pickaxis.grid.datamine.SendMetricsTask;
 import java.util.ConcurrentModificationException;
 import java.util.logging.Level;
 import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
 import org.bukkit.World;
 import org.bukkit.entity.EntityType;
 
@@ -25,11 +25,6 @@ public class WorldMetrics extends AbstractMetricTask
             for( World world : Bukkit.getServer().getWorlds() )
             {
                 this.getClient().gauge( "players.online", world.getPlayers().size(), "world:" + world.getName() );
-                
-                if( DataMinePlugin.getInstance().getSyncTask() instanceof SendMetricsTask )
-                {
-                    this.getClient().gauge( "world.tiles", ( (WorldSyncMetrics) DataMinePlugin.getInstance().getSyncTask().getCollectors().get( WorldSyncMetrics.class ) ).getLastTileCounts().get( world ), "world:" + world.getName() );
-                }
                 
                 this.getClient().gauge( "world.chunks", world.getLoadedChunks().length, "world:" + world.getName() );
                 
--- src/main/java/com/pickaxis/grid/datamine/metrics/tasks/WorldSyncMetrics.java
+++ src/main/java/com/pickaxis/grid/datamine/metrics/tasks/WorldSyncMetrics.java
@@ -1,44 +1,35 @@
 package com.pickaxis.grid.datamine.metrics.tasks;
 
 import com.pickaxis.grid.datamine.DataMinePlugin;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ConcurrentModificationException;
 import java.util.logging.Level;
-import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.World;
+import org.bukkit.entity.EntityType;
 
 /**
  * Sends (synchronously collected) world metrics.
  */
 public class WorldSyncMetrics extends AbstractMetricTask
 {
-    @Getter
-    private final Map<World, Integer> lastTileCounts;
-    
-    public WorldSyncMetrics()
-    {
-        this.lastTileCounts = new HashMap<>();
-    }
-    
     @Override
     public void collect()
     {
         long startTime = System.nanoTime();
         
-        this.getLastTileCounts().clear();
-        
         for( World world : Bukkit.getServer().getWorlds() )
         {
-            int tiles = 0;
-            
             try
             {
+                int tiles = 0;
+                
                 for( Chunk chunk : world.getLoadedChunks() )
                 {
                     tiles += chunk.getTileEntities().length;
                 }
+                
+                this.getClient().gauge( "world.tiles", tiles, "world:" + world.getName() );
             }
             catch( ClassCastException ex )
             {
@@ -48,8 +39,6 @@ public class WorldSyncMetrics extends AbstractMetricTask
             {
                 DataMinePlugin.getInstance().getLogger().log( Level.INFO, "Entity added asynchronously while iterating tiles." );
             }
-            
-            this.getLastTileCounts().put( world, tiles );
         }
         
         if( DataMinePlugin.getInstance().isDebug() )
